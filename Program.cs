ec9b0e27d65d3e11911806195042a9754a27be6b
PavlovIgor1 <f4eze4@mail.ru>
2016-09-15 23:24:50 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // first try to commit
+            Console.WriteLine("Hello, World");
+            //2
+        }
+    }
+}
